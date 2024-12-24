19114af139dbc3bd7a162b04128f92a84a62832d
Liam-Ralph <liamralph42@gmail.com>
2024-12-23 20:46:34 -0500

    change .gitignore to add C# file, correct README.md

---
+++ C Sharp Color Example/Program.cs
@@ -0,0 +1,39 @@
+﻿class CSharpColorTest {
+
+    public static void Main(string[] args){
+
+        // Foreground/Background Selection
+        bool colorBackground = true;
+        string positionControl = "48";
+        if(!colorBackground){
+            positionControl = "38";
+        }
+        
+        // First Sixteen Colors
+        for(int i = 0; i < 16; i++){
+            Console.Write("\u001b[" + positionControl + ";5;" + i + "m " + i + " ");
+            if(i < 10){
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine("\u001b[0m");
+
+        // Remaining Colors
+        for(int i = 16; i < 256; i++){
+
+            // Print the color, extra space if needed
+            Console.Write("\u001b[" + positionControl + ";5;" + i + "m " + i + " ");
+            if(i < 100){
+                Console.Write(" ");
+            }
+
+            // New line every 36 colors, adjust for the first 16
+            if((i - 15) % 36 == 0){
+                Console.WriteLine("\u001b[0m");
+            }
+
+        }
+        Console.WriteLine("\u001b[0m");
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Shows all 256 ASCII colors, useful for adding colored text and backgrounds to text-based project.
+Shows all 256 ANSI colors, useful for adding colored text and backgrounds to text-based project.
 Includes versions in C#, Python, and Java.
-Information on ASCII command lines can be found here (not my website): https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
+Information on ANSI command lines can be found here (not my website): https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
